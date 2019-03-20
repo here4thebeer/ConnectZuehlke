a6deebf863153e85ea782783611177182bcd419e
Don Kodiyan <don.kodiyan@gmail.com>
2019-03-20 17:22:04 +0100

    doko: add springfox dependency

--- backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/ConnectZuehlkeApplication.java
+++ backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/ConnectZuehlkeApplication.java
@@ -2,8 +2,10 @@ package ch.zuehlke.fullstack.ConnectZuehlke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ConnectZuehlkeApplication {
 
     public static void main(String[] args) {
