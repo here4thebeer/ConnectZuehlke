82b3dbae43c5fd209ace4f074733eaeef98f880e
s-mueller <mail@sebastian-mueller.li>
2019-03-20 17:42:35 +0100

    add project DTOs

--- backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/domain/Project.java
+++ backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/domain/Project.java
@@ -5,4 +5,17 @@ public class Project {
     private String title;
     private Location location;
 
+    public Project(String title, Location location) {
+        this.title = title;
+        this.location = location;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
 }
