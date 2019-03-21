14865a4e9d6ad6ca2a77e9ace8694405e372ae62
ynnckth <yannickstreit@hotmail.com>
2019-03-21 11:45:30 +0100

    Remove unused frontend components, redirect to map component by default

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,8 +10,6 @@ import {intersectionObserverPreset, LazyLoadImageModule} from 'ng-lazyload-image
 
 
 import {library} from '@fortawesome/fontawesome-svg-core';
-import {EmployeeListComponent} from './employee-list/employee-list.component';
-import {EmployeeComponent} from './employee-list/employee/employee.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {SearchPipe} from './search.pipe';
@@ -30,14 +28,10 @@ import {
 } from '@angular/material';
 import {NavigationComponent} from './navigation/navigation.component';
 import {LayoutModule} from '@angular/cdk/layout';
-import {HackathonHeadlineComponent} from './hackathon-headline/hackathon-headline.component';
 import {SidebarComponent} from './navigation/sidebar/sidebar.component';
 import {TopbarComponent} from './navigation/topbar/topbar.component';
-import {EmployeeDetailComponent} from './employee-detail/employee-detail.component';
 import {MapComponent} from './map/map.component';
 import {AgmCoreModule} from '@agm/core';
-import {CustomerListComponent} from './customer-list/customer-list.component';
-import {CustomerComponent} from './customer-list/customer/customer.component';
 import {ErrorRequestInterceptor} from './common/error-request-interceptor';
 
 
@@ -50,18 +44,12 @@ library.add(faAngular);
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent,
-    EmployeeListComponent,
     PageNotFoundComponent,
     SearchPipe,
     NavigationComponent,
-    HackathonHeadlineComponent,
     SidebarComponent,
     TopbarComponent,
-    EmployeeDetailComponent,
     MapComponent,
-    CustomerListComponent,
-    CustomerComponent
   ],
   imports: [
     BrowserModule,
