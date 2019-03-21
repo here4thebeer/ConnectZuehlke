8d8c8e01e4b0e2bcda1eea69a257b4babc20cef3
Don Kodiyan <don.kodiyan@gmail.com>
2019-03-21 16:14:35 +0100

    doko: load skills

---
+++ backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/apis/insight/dto/SkillDto.java
@@ -0,0 +1,56 @@
+package ch.zuehlke.fullstack.ConnectZuehlke.apis.insight.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Map;
+
+@JsonIgnoreProperties
+@Entity
+public class SkillDto {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String projectCode;
+
+    private String name;
+
+    public SkillDto() {
+    }
+
+    public SkillDto(String projectCode, String name) {
+        this.projectCode = projectCode;
+        this.name = name;
+    }
+
+    @JsonProperty("Skill")
+    private void unpackSkillNameFromNestedObject(JsonNode skill) {
+        name = skill.get("Name").toString();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getProjectCode() {
+        return projectCode;
+    }
+
+    public void setProjectCode(String projectCode) {
+        this.projectCode = projectCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
